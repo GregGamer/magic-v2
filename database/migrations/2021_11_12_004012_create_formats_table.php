33a79b2c7eda1482ac4446ed183e57e242485a50
Gregor <gregamer98@gmail.com>
2021-11-12 01:56:31 +0100

    created new model with migration

---
+++ database/migrations/2021_11_12_004012_create_formats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formats', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('deck_size');
+            $table->integer('starting_life_total');
+            $table->integer('oracle_card_limit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formats');
+    }
+}
